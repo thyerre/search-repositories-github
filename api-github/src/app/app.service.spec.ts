5d26e38dcfc1783d75654bc3497c07065bc1f50c
thyerre <thyerre321@gmail.com>
2022-01-06 22:42:00 -0300

    criando testes no appService

--- api-github/src/app/app.service.spec.ts
+++ api-github/src/app/app.service.spec.ts
@@ -1,3 +1,5 @@
+import { Items, User } from 'src/app/app-interface';
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
@@ -6,8 +8,45 @@ describe('AppService', () => {
   let service: AppService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AppService]
+    });
     service = TestBed.inject(AppService);
+  });
+
+  it('#getInfoUserByUser should return value from observable',
+    (done: DoneFn) => {
+    service.getInfoUserByUser('thyerre').subscribe(({avatar_url, name, location, html_url, login}:User) => {
+
+      const user: User = {
+        avatar_url,
+        name,
+        location,
+        html_url,
+        login
+      };
+
+      expect(user).toEqual({
+        avatar_url: "https://avatars.githubusercontent.com/u/38963922?v=4",
+        name: "Thyerre Rangel Morais da Silva",
+        location: "Acreúna-GO",
+        html_url: "https://github.com/thyerre",
+        login: "thyerre"
+
+      });
+
+      done();
+    });
+  });
+
+  it('#getRepositoryOrStarred should return value from observable',
+    (done: DoneFn) => {
+    service.getRepositoryOrStarred('thyerre', 'repos').subscribe((values:Items[]) => {
+      expect(Array.isArray(values)).toEqual(true);
+      done();
+    });
   });
 
   it('should be created', () => {
--- api-github/src/app/pages/home/home.component.spec.ts
+++ api-github/src/app/pages/home/home.component.spec.ts
@@ -1,3 +1,5 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
@@ -8,7 +10,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [RouterTestingModule, HttpClientModule]
     })
     .compileComponents();
   });
